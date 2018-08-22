ceeec313feb9ddce9e913572f15d5c6b3d47041a
cpslab <cpslab@alienware-01.cs.mcgill.ca>
2018-08-21 21:18:38 -0400

    delete some lines

--- cal_poi_pairwise_relation.py
+++ cal_poi_pairwise_relation.py
@@ -7,9 +7,7 @@ from scipy.sparse import csr_matrix
 
 def cal_place_pairwise_dist(place_coordinates):
     # this method calculates the pair-wise rbf distance
-    # returns a dict that stores the nearest neighbors of a place within a range of distance
     gamma = 60
-    # place_neighbors_ind = get_nearest_places(location_coordinates)
     place_correlation = rbf_kernel(place_coordinates, gamma=gamma)
     np.fill_diagonal(place_correlation, 0)
     place_correlation[place_correlation < 0.1] = 0
